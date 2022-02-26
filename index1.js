81fde23d2906b8881d5ab81d3461bfa54e8a7cd5
Samuel Atz <samuelatz4661@gmail.com>
2022-02-26 12:55:32 -0600

    test 2 from the vscode cloud

--- index1.js
+++ index1.js
@@ -1,14 +1,14 @@
 const paragraphs = document.getElementsByTagName('p');
 console.log("parrafos en el documento", paragraphs.length);
 
-if(paragraphs.length > 0){
+if (paragraphs.length > 0) {
   const paragraph = paragraphs[0];
   paragraph.innerHTML = 'Bienvenidos al bootcamp!';
   console.log(paragraph);
 }
 
-if(paragraphs.length >1){
+if (paragraphs.length > 1) {
   const paragraph = paragraphs[1];
   const fecha = (new Date()).toLocaleString();
-  paragraph.innerHTML = 'parrafos en el documento: '+paragraphs.length + " (" + fecha + ")";
+  paragraph.innerHTML = 'parrafos en el documento: ' + paragraphs.length + " (" + fecha + ")";
 }
