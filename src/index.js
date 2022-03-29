9c2b0b51114747de990eea7d1e4b624fc596db47
atzsamuel <samuelatz4661@gmail.com>
2022-03-29 13:24:46 -0600

    configuracion inicial de webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import "./js/app";
+//import "./js/app";
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+
+const container = document.getElementById("root");
+const App = () => (
+  <div className="app">
+    <h1>hola react</h1>
+  </div>
+);
+
+ReactDom.render(<App />, container);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        test: /.css$/,
+      },
+      {
+        type:"asset",
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+      }
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+//module.exports = config;
