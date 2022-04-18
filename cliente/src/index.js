e5daba0008fbe275b3a1f9af7a33e3952e7c6655
atzsamuel <samuelatz4661@gmail.com>
2022-04-18 08:50:51 -0600

    http request with axios

--- cliente/src/index.js
+++ cliente/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import axios from "axios";
 import App from "./App";
 import store from "./store";
 
@@ -12,3 +13,17 @@ ReactDOM.render(
   </Provider>,
   rootElement
 );
+
+test();
+
+async function test(){
+try {
+  console.log('antes de fetch')
+  const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+  const productos=response.data;
+  console.log('Productos:',productos);
+  console.log('despues de fetch',productos)
+} catch (error) {
+ console.error('error en el request',error);
+}
+}
